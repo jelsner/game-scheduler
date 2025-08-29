98239437341732cbb71a3bbb46e5d4e19051df82
James B. Elsner <james.elsner@gmail.com>
2025-08-29 18:29:21 -0400

    Delete deploy.R

--- deploy.R
+++
@@ -1,6 +0,0 @@
-install.packages("rsconnect")
-library(rsconnect)
-rsconnect::setAccountInfo(name = "jameselsner",
-                          token = "A4855D2093FC612243A597AD9074A195",
-                          secret = "qs/+/rRjqFmRBMv++4TM/UHQObxzdObjRyVxHdfU")
-rsconnect::deployApp()   # uploads the current folder
